7aeb72d8e7fe168517e42a40834b1355e20d35aa
Landon Reese <landon.j.reese@gmail.com>
2023-03-21 11:40:48 -0500

    Notes

--- EncryptionPractice.py
+++ EncryptionPractice.py
@@ -4,24 +4,24 @@
 # means Python automatically identifies whether user entered a string or a number or list. If
 # the input provided is not correct then either syntax error or exception is raised by python.
 #def input():
-    num = input ("Enter number :")
-    print(num)
-    name1 = input("Enter name : ")
-    print(name1)
+#    num = input ("Enter number :")
+#    print(num)
+#    name1 = input("Enter name : ")
+#    print(name1)
 
 # Python ord() function takes string argument of a single Unicode character and return its
 # integer Unicode code point value.
 #def ord():
-    x = ord('A')
-    print(x)
-    print(ord('ć'))
-    print(ord('ç'))
-    print(ord('$'))
+#    x = ord('A')
+#    print(x)
+#    print(ord('ć'))
+#    print(ord('ç'))
+#    print(ord('$'))
     
 #Python chr() function takes integer argument and return the string representing a character
 # at that code point.
 #def chr():
-    y = chr(65)
-    print(y)
-    print(chr(123))
-    print(chr(36))
+#    y = chr(65)
+#    print(y)
+#    print(chr(123))
+#    print(chr(36))
