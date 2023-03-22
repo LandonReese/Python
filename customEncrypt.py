5c19c18e4064a958019063fe0bebbf80123b9cb2
Landon Reese <landon.j.reese@gmail.com>
2023-03-22 14:02:33 -0500

    Version 2.0

--- customEncrypt.py
+++ customEncrypt.py
@@ -8,7 +8,7 @@ def customEncrypt(text, numN, numD):
     reverseText = "".join(reversed(text))
 
     # 2. Shift all the ASCII characters in reversed input text by “N” positions.
-    for char in text:
+    for char in reverseText:
         x = ord(char)           # x is the ASCII value of our character
         y = numN * numD               # N * D = Our shift amount in the positive or negative direction
         x += y                  # x is now an incremented ascii value
@@ -20,6 +20,6 @@ def customEncrypt(text, numN, numD):
         
         character = chr(x)
         encryptedText = encryptedText + character
-        print("Text:    " + text)
-        print("Encrypt: " + encryptedText)
+        # print("Text:    " + text)
+        # print("Encrypt: " + encryptedText)
     return encryptedText
