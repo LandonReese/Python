2ecc784f0767faa68cc800a0cd8c9ac8fde10ca7
Landon Reese <landon.j.reese@gmail.com>
2023-04-05 21:59:36 -0500

    Re-organized Folders

---
+++ test.py
@@ -0,0 +1,17 @@
+a = 327.41
+b = 325 #250
+c = 939.38 #k stain ww
+d = 0 #
+e = 0 #
+f = 767 #bk 
+g = 188.90 #d pr
+
+
+print("top", a + b)
+print("K st", c - a)
+print()
+print("bk", f - g)
+
+
+
+#print("", a)
