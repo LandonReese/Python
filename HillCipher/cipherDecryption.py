0326e6276bea7ccbccf386a4a7d3750a8c049f8f
Landon Reese <landon.j.reese@gmail.com>
2023-04-06 00:02:17 -0500

    Added Files

---
+++ HillCipher/cipherDecryption.py
@@ -0,0 +1,40 @@
+def cipher_decryption(cipher, key):   
+    # Handle condition ifthe message length is odd.
+    # #<Enter code here>
+    
+    # Convert msg to matrices
+    # <Enter code here>
+    
+    # Convert key to 2x2
+    # #<Enter code here>
+    
+    # finding determinant
+    # <Enter code here>
+    # finding multiplicative inverse
+    #<Enter code here>
+    
+    # find transpose
+    # find minor
+    # <Enter code here>
+    
+    # #changing signs
+    # #<Enter code here>
+    
+    # multiplying multiplicative inverse with adjugate matrix
+    #<Enter code here>
+    
+    # Calculate modulo
+    #<Enter code here>
+    
+    # Convert cipher to plaintext
+    # #<Enter code here>
+    
+    print("Decrypted text: {}".format(decryp_text))
+    
+    #Example:
+    # plaintext = "Secret Message"
+    # plaintext = plaintext.upper().replace(" ","")
+    # key = "hill"
+    # key = key.upper().replace(" ","")
+    # ciphertext = cipher_encryption(plaintext, key)
+    # cipher_decryption(ciphertext, key)
---
+++ HillCipher/cipherEncryption.py
@@ -0,0 +1,26 @@
+import sys
+import numpy as np
+def cipher_encryption(plain, key):
+    
+    # Handle condition if the message length is odd.
+    # <Enter code here>
+    
+    # Convert msg to matrices
+    # #Enter code here>
+    
+    # Convert key to 2x2
+    # #Enter code here>
+    
+    print (key2d)
+    
+    # checking validity of the key
+    # finding determinant
+    # #Enter code here>
+    
+    # finding multiplicative inverse and implementing steps to encrypt text
+    # <Enter code here>
+    # print("Encrypted text: {}".format(encryp_text))
+    
+    
+    
+    return encryp_text
