378858ff5b1537f3381cd6501f3ac2f2063b9e98
Landon Reese <landon.j.reese@gmail.com>
2023-04-27 00:01:53 -0500

    Added Task 1

--- lab6/dh.py
+++ lab6/dh.py
@@ -13,23 +13,27 @@ def power(a, b, p):
 # and a privateKey chosen by the sender 
 # def dh_generatePublicKey(P,G,privateKey): 
 #     <Enter your code here> 
-def dh_generatePublicKey(P,G,privateKey):
+def dh_generatePublicKey(P, G, privateKey):
+    return pow(G, privateKey, P)
     #Your code for this function (copy from your lab5 submission)
-    return(x)
     
 # #Write Function 3 that generates and returns a private key using the 
 # publicKey, privateKey and P chosen by the sender 
 # def dh_generateSecretKey(publicKey, privateKey, P): 
 #     <Enter your code here>
 def dh_generateSecretKey(publicKey, privateKey, P):
-    #Your code for this function (copy from your lab5 submission)
-    return(k)
+    return pow(publicKey, privateKey, P)
     
 # Main Function
 def main(): 
-    P = 0; G = 0; x = 0; a = x; 
-    y = 0; b = 0; 
-    ka = 0; kb = 0; 
+    P = 0; 
+    G = 0; 
+    x = 0; #x is Alice's public key
+    a = x; 
+    y = 0; #y is Bob's public key
+    b = 0; 
+    ka = 0; #ka is Alice's secret key
+    kb = 0; #ka is Bob's secret key
    
     # Both the users will be agreed upon the public keys G and P   
     P = 23; # A prime number P is taken  
@@ -43,17 +47,21 @@ def main():
     print("The private key a for Alice:", a);  
     # Enter code here which calls the appropriate function from above 
     # to generate public key for Alice 
+    x = dh_generatePublicKey(P, G, a)
        
     # Bob will choose the private key b  
     b = 3; # b is the chosen private key  
     print("The private key b for Bob:", b);  
     # Enter code here which calls the appropriate function from above 
     # to generate public key for Bob> 
+    y = dh_generatePublicKey(P, G, b)
   
     # Generating the secret key after the exchange of keys  
     # Enter code here which calls the appropriate function from above 
     # to generate secret keys for both Alice and Bob. Test that these keys 
     # match
+    ka = dh_generateSecretKey(y, a, P)
+    kb = dh_generateSecretKey(x, b, P)
     print("Secret key for the Alice is:", ka);  
     print("Secret Key for the Bob is:", kb);  
         
